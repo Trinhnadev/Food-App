e44010cd248aa099722b3263602cc5e041e32ec6
Trinhnadev <nguyenanhtrinh05@gmail.com>
2024-11-09 21:33:18 +0700

    Create cart page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -21,15 +21,26 @@ const StoreContextProvider = (props)=>{
     }
 
 
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                if (itemInfo) { // Check if itemInfo exists
+                    totalAmount += itemInfo.price * cartItems[item];
+                }
+            }
+        }
+        return totalAmount;
+    }
+    
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount,
     }
     
     return(
